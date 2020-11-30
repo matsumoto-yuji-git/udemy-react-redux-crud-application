7ba1685743e086d6012478e48aaf3a5feffe126f
A_Suzumura A_Suzumura <suzumura38@gmail.com>
2020-11-30 21:00:47 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
+
 function App() {
   return (
-    <div>
-        <h1>Hello,World!</h1>
-    </div>
+    <React.Fragment>
+      <label htmlFor="bar">BAR</label>
+      <input type="text" onChange={() => {console.log("I am clicked.")}} />
+    </React.Fragment>
+  )
+}
+/* JSX -> JavaScript
+function App() {
+  return React.createElement(
+    "div",
+    null,
+    "Hello,World!",
   );
 }
-
+*/
 export default App;
